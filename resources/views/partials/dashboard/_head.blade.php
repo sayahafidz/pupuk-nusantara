14c38b9b13adfe3e99c73f3fcd385b3cb79da17e
Hafidz Alfatih <hafidzalfatih04@gmail.com>
2025-02-18 14:33:48 +0700

    disable cors

--- resources/views/partials/dashboard/_head.blade.php
+++ resources/views/partials/dashboard/_head.blade.php
@@ -1,23 +1,23 @@
 
 
 <!-- Favicon -->
-<link rel="shortcut icon" href="{{secure_asset('images/favicon.ico')}}" />
-<link rel="stylesheet" href="{{secure_asset('css/libs.min.css')}}">
-<link rel="stylesheet" href="{{secure_asset('css/hope-ui.css?v=1.1.0')}}">
-<link rel="stylesheet" href="{{secure_asset('css/custom.css?v=1.1.0')}}">
-<link rel="stylesheet" href="{{secure_asset('css/dark.css?v=1.1.0')}}">
-<link rel="stylesheet" href="{{secure_asset('css/rtl.css?v=1.1.0')}}">
-<link rel="stylesheet" href="{{secure_asset('css/customizer.css?v=1.1.0')}}">
+<link rel="shortcut icon" href="{{asset('images/favicon.ico')}}" />
+<link rel="stylesheet" href="{{asset('css/libs.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/hope-ui.css?v=1.1.0')}}">
+<link rel="stylesheet" href="{{asset('css/custom.css?v=1.1.0')}}">
+<link rel="stylesheet" href="{{asset('css/dark.css?v=1.1.0')}}">
+<link rel="stylesheet" href="{{asset('css/rtl.css?v=1.1.0')}}">
+<link rel="stylesheet" href="{{asset('css/customizer.css?v=1.1.0')}}">
 
 <!-- Fullcalender CSS -->
-<link rel="stylesheet" href="{{ secure_asset('vendor/flatpickr/dist/flatpickr.min.css') }}">
-<link rel='stylesheet' href="{{secure_asset('vendor/fullcalendar/core/main.css')}}" />
-<link rel='stylesheet' href="{{secure_asset('vendor/fullcalendar/daygrid/main.css')}}" />
-<link rel='stylesheet' href="{{secure_asset('vendor/fullcalendar/timegrid/main.css')}}" />
-<link rel='stylesheet' href="{{secure_asset('vendor/fullcalendar/list/main.css')}}" />
-<link rel="stylesheet" href="{{secure_asset('vendor/Leaflet/leaflet.css')}}" />
+<link rel="stylesheet" href="{{ asset('vendor/flatpickr/dist/flatpickr.min.css') }}">
+<link rel='stylesheet' href="{{asset('vendor/fullcalendar/core/main.css')}}" />
+<link rel='stylesheet' href="{{asset('vendor/fullcalendar/daygrid/main.css')}}" />
+<link rel='stylesheet' href="{{asset('vendor/fullcalendar/timegrid/main.css')}}" />
+<link rel='stylesheet' href="{{asset('vendor/fullcalendar/list/main.css')}}" />
+<link rel="stylesheet" href="{{asset('vendor/Leaflet/leaflet.css')}}" />
 
-<link rel="stylesheet" href="{{secure_asset('vendor/aos/dist/aos.css')}}" />
+<link rel="stylesheet" href="{{asset('vendor/aos/dist/aos.css')}}" />
 <style>
     th.hide-search input{
        display: none;
